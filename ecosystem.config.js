f0ec0ea5141593aa09a37cba6fec90aee94181c2
Yan Mendes <yan.ferreira@escale.com.br>
2019-10-05 12:27:25 -0300

    Fix deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'planejar-form-poc',
-      script: 'npm run start:prod',
+      script: 'PORT=4001 npm run start:prod',
       watch: true,
       env_production: {
         NODE_ENV: 'production',
